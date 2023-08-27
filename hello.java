17fe803401b1a82697554fe0302b846ca2d7e829
jayja <jayja@JJTop.unl.edu>
2023-08-27 16:59:57 -0500

    added comment

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 
 public class Hello {
 
+	//prints name and major
+	
 	public static void main(String args[]) {
 		System.out.println("My name is Jay Jay!");
 		System.out.println("My major is Computer Science!");
