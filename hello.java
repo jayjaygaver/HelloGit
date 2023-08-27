5043b80caa64553129b161a540015b4dc99f1a15
johnjacobson <johnjacobson@johns-mbp-3.unl.edu>
2023-08-27 17:39:33 -0500

    added my name and major

--- hello.java
+++ hello.java
@@ -6,6 +6,8 @@ public class Hello {
 	public static void main(String args[]) {
 		System.out.println("My name is Jay Jay!");
 		System.out.println("My major is Computer Science!");
+		System.out.println("My name is John!");
+		System.out.println("My major is Marketing!");
 		
 	}
 }
